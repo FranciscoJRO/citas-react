f51b61afac7aaeb18c80666fd8f084bd5f58a313
FranciscoJRO <francisco.reynoso2000@gmail.com>
2024-02-18 04:16:26 -0600

    Agrega archivos de configuración y componentes nuevos

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+
+
+    return(
+        <header className="grid">
+            <h1 className="text-4xl font-bold uppercase text-indigo-500 mt-5">Desde el Header</h1>
+        </header>
+    )
+
+}
+
+export default Header;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <div>
+      <h2>Listado Pasientes</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
